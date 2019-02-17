b1ba3d72ef549b1848082d9cd4b55537d761d1e0
Richard Feeney <richardfeeney7@gmail.com>
2019-02-17 02:34:48 +0000

    Added comments to display the problem question

---
+++ Problem_1.py
@@ -0,0 +1,9 @@
+""" Author : Richard Feeney 
+    Started: 17/02/2019
+"""
+
+"""                                   Problem 1 :
+        Write a program that asks the user to input any positive integer and outputs the
+        sum of all numbers between one and that number.  
+"""
+
