518d914da6f4bd607a3ecfd99dbd0d26c194a5ba
Richard Feeney <richardfeeney7@gmail.com>
2019-02-17 21:41:47 +0000

    Working on forloop to ask user to enter agin if they input a - num

--- Problem_1.py
+++ Problem_1.py
@@ -8,26 +8,40 @@
 """
 
 print("\n")
-num = int(input ("Please enter a positive number to compute : "))
+num = int(input ("Please enter a positive number : "))
 i = 0
-count=1
+y = 0
 
 while num < 0:
     print("\n")
-    try_again = input("Negative number. Would you like to try another number  'Y' or 'N' ?  ")
-    if try_again == "Y" or try_again == "y":
+    try_again1 = input("Negative number. Would you like to try another number  'Y' or 'N' ? : ")
+    if try_again1 == "Y" or try_again1 == "y":
         print("\n")
-        num = int(input("Please try again : " ))
+        num = int(input("Please try again by entering a positive number : " ))
     else:
         print("\n")
-        print("Thank you for using this application today, see you soon ")
-        break
+        print("Thank you for using this application. I hope that you will use it again see you soon ")
 else: 
     while num > 0:
         i = i + num
         num = num - 1 
+
     else:
         print("\n")
         print("The sum of all the numbers between one and your number is " , i )
         print("\n")
-        print("Thank you for using this application today, see you soon ")
+        try_again = input("Thank you for using the application.Would you like to try another number  'Y' or 'N' ?  ")
+        print("\n")
+        while try_again == "Y" or try_again == "y":
+            print("\n")
+            num = int(input("Please try entering a positive number again : " ))
+            while num > 0:
+                y = y + num
+                num = num - 1 
+            else:
+                print("\n")
+                print("The sum of all the numbers between one and your number is " , y )
+                break
+        else:
+            print("Thank you for using this application. I hope that you will use it again see you soon ")
+    
