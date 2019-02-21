0f7f942e1bbcdb1831f5e5e783a62e87ede96659
Richard Feeney <richardfeeney7@gmail.com>
2019-02-21 19:39:48 +0000

    If statement for Problem 2

--- Problem_1.py
+++ Problem_1.py
@@ -1,8 +1,6 @@
-# Author : Richard Feeney Started: 17/02/2019
+#                   Author : Richard Feeney Started: 17/02/2019
 
-#                                   Problem 1 :
-#        Write a program that asks the user to input any positive integer and outputs the
-#                    sum of all numbers between one and that number.  
+#                                    Problem 1 :                                                                         Write a program that asks the user to input any positive integer and outputs the                                               sum of all numbers between one and that number.  
 
 
 
@@ -18,8 +16,10 @@ i = y = 0
 while num < 0:
     print("\n")
     #The try_again1 variable is used to store the input from the user asking if they want to try again.
+
     try_again1 = input("Negative number. Would you like to try another number  'Y' or 'N' ? : ")
     #The if statment checks if the user enters Y or y and if anything else is entered it will exit the application.
+
     if try_again1 == "Y" or try_again1 == "y":
         print("\n")
         num = int(input("Please try again by entering a positive number : " ))
@@ -27,8 +27,10 @@ while num < 0:
         print("\n")
         print("Thank you for using this application. I hope that you will use it again see you soon ")
         break
+
 #ELSE within the while loop that checks if num is < 0 and if num > 0 it will use the while loop in this section.
 else: 
+
     #WHILE checks is num is > 0 and if so it will add NUM to i and that number will be stored in i and num will be decreased by one and loop until num > 0 and each time i will be updated by added it to the current num.
     while num > 0:
         i = i + num
@@ -41,6 +43,7 @@ else:
         print("\n")
         #try_again will store the responce if the user wants to try again or not. 
         try_again = input("Thank you for using the application.Would you like to try another number  'Y' or 'N' ?  ")
+
         #if the user wants to try again the while loop haas a nested while loop to do the same calculation as the about while num > 0 on line 33 but instead of using i again I have used y. 
         while try_again == "Y" or try_again == "y":
             print("\n")
@@ -48,6 +51,7 @@ else:
             while num > 0:
                 y = y + num
                 num = num - 1 
+                
             #if the user input was not to try again it will out the number in y
             else:
                 print("\n")
---
+++ Problem_2.py
@@ -0,0 +1,8 @@
+# Richard Feeney
+
+import datetime
+
+if datetime.datetime.today().weekday() == 1 or 3:
+  print("Great, Today does begin with the letter T")
+else:
+  print("Unfortunately today does not start with a T :) .")
