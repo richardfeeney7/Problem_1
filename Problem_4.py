ecd225347459f20f60c8fa8777b3685c67ba11f9
Richard Feeney <richardfeeney7@gmail.com>
2019-02-23 11:11:31 +0000

    Added comments

--- Problem_4.py
+++ Problem_4.py
@@ -8,14 +8,19 @@
        if it is even,divide it by two,but if it is odd,multiply it by three and add one.
 '''
 
+print("\n")
 num = int(input("Please enter a positive number : ")) #Store user input in num
+print("\n")
 
-while num > 1:
-    if num % 2 == 0:
-       num = num/2
-       print(num)
 
-    else:
-        num = (num*3) + 1
-        print(num) 
+while num > 1: # This will check user input and ones loop while > 1. If i had > 0 is would run an infinite loop with the number 1
+    if num % 2 == 0: # Checks the remainder of num and if = 0 continue
+       num = num/2 #Divide num by 2 
+       print(int(num)) #The int(input) on line 12 was giving a boolean number so i added int here too
 
+    else: # Will be used if the number is an odd number 
+        num = (num*3) + 1 #If number is odd this calculation will be stored and printed to the screen
+        print(int(num)) 
+
+if num < 1: # Check is num < 1 and prints try again
+    print("Please try again")
