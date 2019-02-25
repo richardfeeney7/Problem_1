6525750564c2677d01c33e8865d3ca8827416cfc
Richard Feeney <richardfeeney7@gmail.com>
2019-02-25 12:47:33 +0000

    Working on if q5

--- Problem_5.py
+++ Problem_5.py
@@ -5,19 +5,30 @@
 #                                     is a prime.       
 
 
+print("\n")
+name = input("Hello and welcome to my application, please input your name ? ")
+print("\n")
 num = int(input("Please enter a positive number : ")) # Accepts the user number to be checked if it is prime
-#y=2 Not needed as I used a range function instead. 
 
-''' if num % y == 0:
-    print("Your number ",num, " is NOT a prime") 
-else:
-    print("Your number ",num, " is a prime")
+'''
+y=2
+if num > 1:
+    if num % y == 0:
+        print("Your number ",num, " is NOT a prime") 
+    else:
+        print("Your number ",num, " is a prime")
 '''
 # If statement above didn't give me the desired output. I have used the range function instead.
+print("\n")
 
-for i in range(2, num): #Create the range function and set i = 2
-    if num % i == 0:    #If the user input Mod i == 0 do this print not prime 
-        print("Sorry but ",num, " is not prime")
-        break
-else:
-    print("Yes ",num, "is a prime number")     
+if num > 1:
+    for i in range(2,num): #Create the range function and set i = 1
+        if num % i == 0:    #If the user input Mod i == 0 do this print not prime 
+            print("Sorry", name, " ",num, " is not prime")
+            break
+    else:
+        print("Yes ", name," ",num, "is a prime number")   
+elif num == 1:
+    print("Sorry ", num, "is not a prime number")  
+elif num < 0:
+    print("A prime number cant be a negative number")
