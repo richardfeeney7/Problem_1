2cef4644beb08b92acc587564e405a764fb6ff68
Richard Feeney <richardfeeney7@gmail.com>
2019-02-25 11:58:55 +0000

    Working on problem 5

--- Problem_5.py
+++ Problem_5.py
@@ -5,12 +5,19 @@
 #                                     is a prime.       
 
 
-x = int(input("Please enter a positive number : "))
-y = 2
+num = int(input("Please enter a positive number : ")) # Accepts the user number to be checked if it is prime
+#y=2 Not needed as I used a range function instead. 
 
+''' if num % y == 0:
+    print("Your number ",num, " is NOT a prime") 
+else:
+    print("Your number ",num, " is a prime")
+'''
+# If statement above didn't give me the desired output. I have used the range function instead.
 
-if x > 0:
-    if x % y == 0:
-        print("Your number ",x, "is prime") 
-    else:
-        print("Not prime")
+for i in range(2, num): #Create the range function and set i = 2
+    if num % i == 0:    #If the user input Mod i == 0 do this print not prime 
+        print("Sorry but ",num, " is not prime")
+        break
+else:
+    print("Yes ",num, "is a prime number")     
