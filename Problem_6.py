3cb155fe22fe45f653e7f468ae9473d7d452915c
Richard Feeney <richardfeeney7@gmail.com>
2019-02-25 17:48:03 +0000

    Question 6 Started

---
+++ Problem_6.py
@@ -0,0 +1,6 @@
+#                   Author : Richard Feeney Started: 25/02/2019
+
+#                                    Problem 6 :    
+#           Write a program that takes a user input string and outputs every second word?
+
+sent = input("Please enter a sentence : ") # Accept user input and store in sent
