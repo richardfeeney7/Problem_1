e6869ce64169ccbc9c9ccc38cf3be9a696c4555e
Richard Feeney <richardfeeney7@gmail.com>
2019-03-02 13:00:42 +0000

    Added function

--- Problem_5.py
+++ Problem_5.py
@@ -4,12 +4,6 @@
 # Write a program that asks the user to input a positive integer and tells the user whether or not the number 
 #                                     is a prime.       
 
-
-print("\n")
-name = input("Hello and welcome to my application, please input your name ? ")
-print("\n")
-num = int(input("Please enter a positive number : ")) # Accepts the user number to be checked if it is prime
-
 '''
 y=2
 if num > 1:
@@ -17,18 +11,25 @@ if num > 1:
         print("Your number ",num, " is NOT a prime") 
     else:
         print("Your number ",num, " is a prime")
-'''
-# If statement above didn't give me the desired output. I have used the range function instead.
-print("\n")
+'''# If statement above didn't give me the desired output. I have used the range function instead.
 
-if num > 1:
-    for i in range(2,num): #Create the range function and set i = 1
-        if num % i == 0:    #If the user input Mod i == 0 do this print not prime 
-            print("Sorry", name ,num, "is not prime")
-            break
-    else:
-        print("Yes", name, num, "is a prime number")   
-elif num == 1: # 1 cant be a prime number aas it only has 1 factor
-    print("Sorry", num, "is not a prime number")  
-elif num < 0:
-    print("A prime number cant be a negative number")
+
+
+def prime(num):                 # Define the function
+    if num > 1:                 # Start if statement to search for prime number
+        for i in range(2,num):  # Create the range function and set i = 1
+            if num % i == 0:    # If the user input Mod i == 0 do this print not prime 
+                print("Sorry", name ,num, "is not prime")
+                break           # Break out one number is not prime
+        else:
+            print("Yes", name, num, "is a prime number") 
+
+    elif num == 1:              # 1 cant be a prime number as it only has 1 factor
+        print("Sorry", num, "is not a prime number")  
+
+    elif num < 0:               # A prime number cant be a negative number
+        print("A prime number cant be a negative number")
+
+
+name = input("Hello and welcome to my application, please input your name ? ")
+prime(int(input("Please enter a positive number : "))) # Accepts the user number and sends to function
