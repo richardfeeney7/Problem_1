140e42d08bc953fa58af63105e5030f49caad569
Richard Feeney <richardfeeney7@gmail.com>
2019-03-02 12:46:59 +0000

    Added Comments

--- Problem_8.py
+++ Problem_8.py
@@ -4,12 +4,10 @@
 #      Write a program that outputs today’s date and time in the format “Monday,January 10th 2019 at 1:15pm”. 
 
 
-import datetime 
+import datetime #Import the datetime module
 
-# Use the satetime object to use in strings
-current = datetime.datetime.now()
+current = datetime.datetime.now() # Use datetime.no() to get the current date and time
 print("\n")
 
-print("The current date and time is : ",(current.strftime("%A, %B, %Y at %I:%M %p")))
-# strftime is used format / pull different information from date by using the %? 
+print("The current date and time is :",(current.strftime("%A, %B, %Y at %I:%M %p"))) # strftime is used format / pull different information from date by using the %? 
 
