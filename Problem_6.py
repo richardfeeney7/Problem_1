7cea431b9b130613b64b34fcdbaac7c3f5582c63
Richard Feeney <richardfeeney7@gmail.com>
2019-03-02 12:42:08 +0000

    Added Comments

--- Problem_6.py
+++ Problem_6.py
@@ -4,18 +4,13 @@
 #           Write a program that takes a user input string and outputs every second word?
 
 
-sent = input("Please enter a sentence : ").split(' ') # Accept user input and store in sent
-# Split is used to split the string into a list
+sent = input("Please enter a sentence : ").split(' ') # Accept user input and store in sent & Split is used to split the string into a list
 
-sent = [ignore.replace(',',  '').replace('.' , '') for ignore in sent]
+sent = [ignore.replace(',',  '').replace('.' , '') for ignore in sent] # This lne of code will ignore input of , & . 
 
-#     sent[start:end:step]  How to slice the string sent. 
-print(*sent[0:len(sent):2], sep=" / ",) #slicing the string to pull the information needed. len () will 
-# sep is used to add a seperation with lines between the work. Not needed but I think it reads better
-
-
-
-
-# To remove [] from the output I have added a * just before sent in the pint statement but the statement
-# using the .join does the same thing and puts a comma between the words.
-# print (', '.join(sent[1:len(sent):2]))
+      #sent[start:end:step] How to slice the string sent. 
+print(*sent[0:len(sent):2], sep=" / ",) # Slicing the string to pull the information needed. 
+                                        # ep is used to add a / between words. 
+                                        # To remove [] from the output I have added a * just before sent in the pint statement
+# Using the .join does the same thing and puts a comma between the words.
+# Print (', '.join(sent[1:len(sent):2]))
