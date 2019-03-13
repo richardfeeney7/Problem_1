713a659f4c47746782cecbb11cd1ae63f762425e
Richard Feeney <richardfeeney7@gmail.com>
2019-03-13 20:27:24 +0000

    Finished

--- Problem_1.py
+++ Problem_1.py
@@ -1,17 +1,21 @@
 #                   Author : Richard Feeney Started: 17/02/2019
 
-#                                    Problem 1 :                                                                         
-#          Write a program that asks the user to input any positive integer and outputs the sum of all numbers between one and that number.  
+#                                    Problem 1 :   
+#                                                                       
+#  Write a program that asks the user to input any positive integer and outputs the sum of all numbers between one and that number.  
 
-def pos_num(num) :
+
+'''User input is stored in num. And i and y are set to 0, y will be used to store user input when they are asked if they want to try again '''
+
+def pos_num(num) :  # Define the function that will accept user input and store it in num
     i = y = 0       # Set i and y to 0
     while num < 0:  # Loop will check num to see if it it less than 0.
         print("\n")
 
-        #The try_again1 variable is used to store the input from the user asking if they want to try again.
+        #The try_again1 variable is used to store the input from the user when asked if they want to try again.
         try_again1 = input("Negative number. Would you like to try another number  'Y' or 'N' ? : ")
 
-        #The if statement checks if the user enters Y or y and if anything else is entered it will exit the application.
+        #The if statement checks if the user enters Y or y, if anything else is entered it will exit the application.
         if try_again1 == "Y" or try_again1 == "y":
             print("\n")
             num = int(input("Please try again by entering a positive number : " ))
@@ -20,23 +24,23 @@ def pos_num(num) :
             print("Thank you for using this application. I hope that you will use it again see you soon ")
             break
 
-    #ELSE within the while loop that checks if num is < 0 and if num > 0 it will use the while loop in this section.
     else: 
 
-        #WHILE checks is num is > 0 and if so it will add NUM to i and that number will be stored in i and num will be decreased by one and loop until num > 0 and each time i will be updated by added it to the current num.
+        # WHILE checks is num > 0,if True it will add num to i and this will be stored in i then num will be decreased by one and loop until num > 0 and each time i will be updated by added it to the current num.
         while num > 0:
             i = i + num
             num = num - 1 
-        
-        #This ELSE is whit in the WHILE num > 0 to output the final number for i and ask if they want to try again.
+            #print("The sum of all the numbers between one and your number is " , i ) # Moved to line 33 as multiple lines were outputting
+
         else:
             print("\n")
             print("The sum of all the numbers between one and your number is " , i )
             print("\n")
-            #try_again will store the responce if the user wants to try again or not. 
+
+            #try_again will store the responce when the user is asked if they want to try again. 
             try_again = input("Thank you for using the application.Would you like to try another number  'Y' or 'N' ?  ")
 
-            #if the user wants to try again the while loop haas a nested while loop to do the same calculation as the about while num > 0 on line 33 but instead of using i again I have used y. 
+            #if the user wants to try again the while loop has a nested while loop to do a similar calculation to the the loop at line 26
             while try_again == "Y" or try_again == "y":
                 print("\n")
                 num = int(input("Please try entering a positive number again : " ))
@@ -44,18 +48,15 @@ def pos_num(num) :
                     y = y + num
                     num = num - 1 
 
-
-                #if the user input was not to try again it will out the number in y
                 else:
                     print("\n")
                     print("The sum of all the numbers between one and your number is " , y )
                     break
+
             # Thank the user for using the application 
             else:
                 print("\n")
                 print("Thank you for using this application. I hope that you will use it again see you soon ")
 
-# User input is stored in num. And i and y are set to 0, y will be used to store user input when they are asked if they want to try 
-# again if its a negative number and i will be used to store num -1. #
-pos_num(int(input ("Please enter a positive number : ")))
+pos_num(int(input ("Please enter a positive number : "))) # Send number to the function
     
