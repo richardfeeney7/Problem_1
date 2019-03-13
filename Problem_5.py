5336224cc80687b0a4b99452ff78c42382984dde
Richard Feeney <richardfeeney7@gmail.com>
2019-03-13 17:44:36 +0000

    Addding comments

--- Problem_5.py
+++ Problem_5.py
@@ -4,6 +4,9 @@
 # Write a program that asks the user to input a positive integer and tells the user whether or not the number 
 #                                     is a prime.       
 
+
+
+# If statement in comments below didn't give me the desired output. I needed to insert a range function.
 '''
 y=2
 if num > 1:
@@ -11,21 +14,20 @@ if num > 1:
         print("Your number ",num, " is NOT a prime") 
     else:
         print("Your number ",num, " is a prime")
-'''# If statement above didn't give me the desired output. I have used the range function instead.
-
+'''
 
 
 def prime(num):                 # Define the function
-    if num > 1:                 # Start if statement to search for prime number
-        for i in range(2,num):  # Create the range function and set i = 1
-            if num % i == 0:    # If the user input Mod i == 0 do this print not prime 
-                
+    if num > 1:                 # Start IF statement to search for prime number
+        for i in range(2,num):  # Create the range function starting at 2 that also accepts the user input paramater
+            if num % i == 0:    # Check if user input % 1 = 0, if true continue.
+
                 #print("Sorry" ,name, num, "is not prime")
-                print(f"Sorry {name}" ,num, "is not prime") # Modified above print to use formatted string
-                break                                       # Break out one number is not prime
+                print(f"Sorry {name}," ,num, "is not prime") # Modified above print to use formatted string
+                break                                       # Break out of statement
         else:
             # print("Yes", name, num, "is a prime number") 
-              print(f"Yes {name}", num, "is a prime number") # Modified above print to use formatted string
+              print(f"Sucess {name},",num,"is a prime number") # Modified above print to use formatted string
 
     elif num == 1:              # 1 cant be a prime number as it only has 1 factor
         print("Sorry", num, "is not a prime number")  
