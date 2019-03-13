9875141e2efc7b16b4834de06b4c62c917ae6e7c
Richard Feeney <richardfeeney7@gmail.com>
2019-03-13 17:34:16 +0000

    Comments

--- Problem_6.py
+++ Problem_6.py
@@ -4,13 +4,16 @@
 #           Write a program that takes a user input string and outputs every second word?
 
 
-sent = input("Please enter a sentence : ").split(' ') # Accept user input and store in sent & Split is used to split the string into a list
+sent = input("Please enter a sentence : ").split(' ') # Accept user input and store in sent & Split is used to break the string into a list
 
-sent = [ignore.replace(',',  '').replace('.' , '') for ignore in sent] # This lne of code will ignore input of , & . 
+sent = [ignore.replace(',',  '').replace('.' , '') for ignore in sent] # This line of code is used to ignore input of " , & ."  in the user input that is stored in sent. 
 
       #sent[start:end:step] How to slice the string sent. 
 print(*sent[0:len(sent):2], sep=" / ",) # Slicing the string to pull the information needed. 
-                                        # ep is used to add a / between words. 
+                                        # sep is used to add a / between words, just for easier reading 
                                         # To remove [] from the output I have added a * just before sent in the pint statement
-# Using the .join does the same thing and puts a comma between the words.
-# Print (', '.join(sent[1:len(sent):2]))
+
+'''
+In the print statement on line 12 we can use ".join" either, the code for this is below.
+print (', '.join(sent[1:len(sent):2]))
+'''
