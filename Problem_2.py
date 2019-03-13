dbd1897509ab946e1f8bf2a93c6ff25950d6f846
Richard Feeney <richardfeeney7@gmail.com>
2019-03-13 20:52:46 +0000

    Finished

--- Problem_2.py
+++ Problem_2.py
@@ -8,28 +8,28 @@
 
 import datetime as dt #Import the datetime module and give it an alias
 
-d = dt.datetime.now() #This will input the current date into d where i will use in my else print statement
+d = dt.datetime.now() #This will input the current date ans store it in d.
 #print(d) # Testing to see the date format and how it looks
 
 tdate = dt.datetime.today().weekday() #This will find todays date and store it in tdate where i will uses later.
-#print(tdate) # Testing to see the output from dt.datetime.today().weekday(). It will output a number
+#print(tdate) # Testing to see the output from dt.datetime.today().weekday(). It will output a number to represent the day of the week.
 
 print("Hello and welcome to the application that will tell you if the day begins with a letter T" )
 print("\n")   
 
-
 ''' if datetime.datetime.today().weekday() == 1 or datetime.datetime.today().weekday() == 3:
     
-    I created a variable called tdate to use in the active if statement as the commented out if on line 21 statement was very long.
-    This will use the date starting from position 0 = Monday, 1 = Tuesday etc up to 6 that is = Sunday,
-    isoweekday could be used that would start from 1 = Monday to 7 = Sunday
+    I created a variable called tdate to use in the active if statement below because the if statement commented out on line 20 statement was long and could be shortened
+    and this will generate the day starting from position 0 = Monday, 1 = Tuesday etc up to 6 that is = Sunday,
+    
+    isoweekday could be used that would start from 0 = Monday to 6 = Sunday
 '''
 
-if tdate == 1 or tdate ==3: # Instead of using the longer if above i created a var tdate to store current date.
+if tdate == 1 or tdate ==3: # Finds the days by their position from 0 Monday to 6 Sunday
 
-    print(" Today starts with a T")# I orginally used tdate in this print but change to use the strftime method instead
-    print("Great news :) todays is" ,d.strftime("%A"), "and begins with the letter T") #This will tell the user what day it is and that it does not start with a T
-    # Steftime("%A") will display the full weekday name and if its Tue or Thurs is will output 
+    print(" Today starts with a T") # I orginally used tdate in this print but change to use the strftime method instead
+    print("Great news :) todays is" ,d.strftime("%A"), "and begins with the letter T") # This will tell the user what day it is and that it starts with a T
+    # Steftime("%A") will display the full weekday name and if its Tue or Thurs
     
 else:
     print("Unfortunately today is",d.strftime("%A"), "does not start with a T :) .") 
