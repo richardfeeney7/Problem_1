6351532485cd0e03acf03527ceac7851b6df21a0
Richard Feeney <richardfeeney7@gmail.com>
2019-03-13 14:00:56 +0000

    Comments

--- Problem_9.py
+++ Problem_9.py
@@ -1,19 +1,15 @@
-#f = open("Q9_File.txt", "r") # Open the TXT file
-#f = open("Q9_File.txt", "w") # Write to the TXT file. This will overwrite what is in the file already. 
-#f = open("Q9_File.txt", "a") # Use a instead of w to not overwrite it but instead append the file.
-#f = open("Q9_File.txt", "r+") # Read and write to a file
-#s = f.read() # Read the file
+#                   Author : Richard Feeney Started: 10/0232019
 
-# f.write ("Example how to write to \r\n a file") # Write to the file with some on a new line
+#                                    Problem 9 :    
+#      Write a program that reads in a text ﬁle and outputs every second line. ”. 
 
-#print(s)
 
-#f.close()  # Close the File, not required but it is good practice
 
-#Below with will write the text to the file
-print("\n")
+
+# Below f.writes have already written these lines to my text file so I have commented them out. 
+'''
 with open("Q9_File.txt", "w") as f: #recommended way to use as it will close once the indent finishes.
-        f.write("TITLE  :  What is Python \n")
+        f.write("Title  :  What is Python \n")
         f.write("Python is an interpreted, object-oriented, high-level programming language with dynamic semantics. \n")
         f.write("Its high-level built in data structures, combined with dynamic typing and dynamic binding, make it very attractive for Rapid Application Development, as well as for use as ascripting or glue language to connect existing components together. \n") 
         f.write("Python's simple, easy to learn syntax emphasizes readability and therefore reduces the cost of program maintenance. \n")
@@ -25,9 +21,10 @@ with open("Q9_File.txt", "w") as f: #recommended way to use as it will close onc
         f.write("This form of analysis is just one of the many steps that must be completed when conducting a research experiment.\n")
         f.write("Data from various sources is gathered, reviewed, and then analyzed to form some sort of finding or conclusion.\n")
         f.write("There are a variety of specific data analysis method, some of which include data mining, text analytics, business intelligence, and data visualizations. \n")
-
 '''
+
 #Below with will read the file and output it to the screen
+'''
 with open("Q9_File.txt", "r") as f:
     s = f.read()
 print(s)
@@ -46,11 +43,11 @@ with open("Q9_File.txt", "r") as f:
 
 # Rewritten block of code to not include the line spacing on line 22. Please note that I have added a second line space into line 22 as I want line 23 to output. 
 with open("Q9_File.txt", "r") as f:
-    count = 0
-    for line in f:
+    count = 2                   # Set a variable to 2 that will used in the elif 
+    for line in f:              # For loop used to increment count by one as it loops the file f
         count += 1
-        if not line.strip(): 
+        if not line.strip():    # If the line is empty continue will just pass it and not display it as output
             continue
-        elif count % 2 == 1:
+        elif count % 2 == 1:    # Used to indicate a remander and if it is 1 it will print that line
             print(line)
 
