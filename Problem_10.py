5fcb40655dbacf0f38ca182440c23b956dc03916
Richard Feeney <richardfeeney7@gmail.com>
2019-03-14 11:13:31 +0000

    Working on Question 10

---
+++ Problem_10.py
@@ -0,0 +1,6 @@
+#                   Author : Richard Feeney Started: 14/03/2019
+
+#                                    Problem 10 : 
+#    
+#      Write a program that displays a plot of the functions x, x2 and 2x in the range [0,4].
+ 
